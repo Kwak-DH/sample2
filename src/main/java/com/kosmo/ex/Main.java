9486f148ed8582d5d25e44fd7cab44f6442ac689
KOSMO <eogns6595@gmail.com>
2024-11-12 10:57:56 +0900

    첫 커밋 메시지

--- src/main/java/com/kosmo/ex/Main.java
+++ src/main/java/com/kosmo/ex/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("안녕하세요");
         System.out.println("이것은 곽대훈이 만든 로직");
         System.out.println("뿌까뿌까");
+        System.out.println("이것은 곽대훈이 만든 로직2");
     }
 }
